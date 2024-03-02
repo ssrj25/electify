0f45d1f67a287ece923965bb0d07da0aa773a736
Safdar <787alisniazi787@gmail.com>
2024-03-02 19:40:56 +0530

    fix package

---
+++ src/components/Result.js
@@ -0,0 +1,76 @@
+import React from 'react'
+
+const Result = () => {
+  return (
+    <>
+        <div className="container text-center">
+  <h1>Winner's List</h1>
+  <div className="row align-items-start">
+    <div className="col">
+      <h2>Mumbai</h2>
+      <div className="cardiie" style={{ width: "18rem" }}>
+        <img src="Img22.jpg" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <h6 className="count">... Votes</h6>
+          {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+          {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+        </div>
+      </div>
+      <div className="cardiie" style={{ width: "18rem" }}>
+        <img src="Img22.jpg" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <h6 className="count">... Votes</h6>
+          {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+          {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+        </div>
+      </div>
+      <div className="cardiie" style={{ width: "18rem" }}>
+        <img src="Img22.jpg" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <h6 className="count">...Votes</h6>
+          {/* <p class="card-text"></p> */}
+          {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+        </div>
+      </div>
+    </div>
+    <div className="col">
+      <h2>Delhi</h2>
+      <div className="cardiie" style={{ width: "18rem" }}>
+        <img src="Img22.jpg" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+          {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+          <h6 className="count">... Votes</h6>
+        </div>
+      </div>
+      <div className="cardiie" style={{ width: "18rem" }}>
+        <img src="Img22.jpg" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+          {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+          <h6 className="count">... Votes</h6>
+        </div>
+      </div>
+      <div className="cardiie" style={{ width: "18rem" }}>
+        <img src="Img22.jpg" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <h6 className="count">... Votes</h6>
+          {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+          {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </>
+  )
+}
+
+export default Result
