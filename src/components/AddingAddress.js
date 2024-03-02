5df66eda17544726abadc0a9b7b1c4d128c2697f
Hriday Desai <126891488+Dogtags27@users.noreply.github.com>
2024-03-03 04:01:17 +0530

    Update AddingAddress.js

--- src/components/AddingAddress.js
+++ src/components/AddingAddress.js
@@ -1,55 +1,3 @@
-// import React from 'react';
-// import { Link }from 'react-router-dom';
-
-// const AddingAddress = () => {
-
-
-//   return (
-//     <>
-//       <div className="container col-xxl-8 px-4 py-5">
-//         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-//           <div className="col-10 col-sm-8 col-lg-6">
-//             <form>
-//               <div className="mb-3">
-//                 <input
-//                   placeholder='Example: njbfsdkfudfhbxjbzxvsdjfhsdilhcbzxmvjh'
-//                   type="text"
-//                   className="form-control form-control-lg border shadow-lg bg-info-subtle"
-//                   id="exampleInputEmail1"
-//                   aria-describedby="emailHelp"
-//                 />
-//                 <div id="emailHelp" className="form-text">
-//                   We'll never share your address with anyone else.
-//                 </div>
-//               </div>
-//               <div className="mb-3 form-check">
-//                 <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-//                 <label className="form-check-label" htmlFor="exampleCheck1">
-//                   I agree with the terms and conditions
-//                 </label>
-//               </div>
-//               <Link type="submit "  to ="/verify" className="btn btn-primary">
-//                 Submit
-//               </Link>
-//             </form>
-//           </div>
-//           <div className="col-lg-6">
-//             <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">
-//               Enter Your Address
-//             </h1>
-//             <p className="lead">
-//               The Voting System is designed to cast votes with maximum security to avoid cheating in elections. The system is based on the latest Blockchain technology. It ensures that each person is allowed to vote only once.
-//             </p>
-           
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default AddingAddress;
-
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -129,5 +77,3 @@ const AddingAddress = () => {
     </>
   );
 };
-
-export default AddingAddress;
